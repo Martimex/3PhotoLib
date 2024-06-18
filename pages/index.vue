ed2c83003dbf4e9ab2dfda1fd3397ad504394dce
Martimex <kamilmarciniak533@wp.pl>
2024-06-18 21:20:11 +0200

    chore: refactor home page styling
    Refactored most of CSS used for Tailwind classes
    where it actually make sense to do so.

--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,10 @@
 
 <template>
     <main class="min-h-screen bg-[#ddd] text-black flex flex-col justify-center relative overflow-hidden">
-<!--         <section class="flex flex-col items-center justify-center">
-            <h1 class="text-5xl font-bold"> 3PhotoLib </h1>
-            <h2 class="text-xl"> Welcome!!! </h2>
-            <div>
-                <button> Log in </button>
-            </div>
-        </section> -->
-        <section class="sectest ">
-            <div class="board backdrop-blur-[2px]">
-                <div class="slide-container">
-                    <div class="slide-box">
+        <section class="absolute top-0 left-0 h-dvh w-screen">
+            <div class="backdrop-blur-[2px] overflow-hidden flex items-center justify-center">
+                <div class="grid place-items-center bg-gradient-to-tr from-[#eee] to-[#ddd] grid-cols-[1fr_1fr_1fr] grid-rows-[auto] gap-6 max-h-dvh">
+                    <div class="[transform:rotateZ(15deg)_translateX(-33%)] flex flex-col w-max justify-center items-center gap-y-6 mr-6">
                         <img class="slide" src="https://images.unsplash.com/photo-1448375240586-882707db888b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwxfHxmb3Jlc3R8ZW58MHx8fHwxNzE4MTIzNTQyfDA&ixlib=rb-4.0.3&q=80&w=1080" />
                         <img class="slide" src="https://images.unsplash.com/photo-1627667928346-5fc86d099a5c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHw0N3x8c3BvcnQlMjBjYXJ8ZW58MHx8fHwxNzE4MTk2MTE3fDA&ixlib=rb-4.0.3&q=80&w=1080" />
                         <img class="slide" src="https://images.unsplash.com/photo-1608178398319-48f814d0750c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwzfHxuZWJ1bGF8ZW58MHx8fHwxNzE4MTk2NTc4fDA&ixlib=rb-4.0.3&q=80&w=1080"/>
@@ -22,7 +15,7 @@
                         <img class="slide" src="https://images.pexels.com/photos/200985/pexels-photo-200985.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" />
                         <img class="slide" src="https://images.unsplash.com/photo-1617581629397-a72507c3de9e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwyNXx8Y2hlcnJ5JTIwYmxvc3NvbXxlbnwwfHx8fDE3MTgyMjMwMjN8MA&ixlib=rb-4.0.3&q=80&w=1080" />
                     </div>
-                    <div class="slide-box">
+                    <div class="[transform:rotateZ(15deg)_translateX(-33%)] flex flex-col w-max justify-center items-center gap-y-6 mr-6">
                         <img class="slide" src="https://images.unsplash.com/photo-1548199973-03cce0bbc87b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwyMHx8ZG9nfGVufDB8fHx8MTcxODIyMjExNnww&ixlib=rb-4.0.3&q=80&w=1080" />
                         <img class="slide" src="https://images.pexels.com/photos/68389/pexels-photo-68389.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" />
                         <img class="slide" src="https://images.unsplash.com/photo-1456553405974-0712f71f2e69?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHw0MHx8bWFuJTIwaGFuZHNvbWV8ZW58MHx8fHwxNzE4MTk1MzQ2fDA&ixlib=rb-4.0.3&q=80&w=1080" />
@@ -30,7 +23,7 @@
                         <img class="slide" src="https://images.unsplash.com/photo-1532274402911-5a369e4c4bb5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHw0fHxsYXZlbmRlciUyMGZpZWxkfGVufDB8fHx8MTcxODIxNTY0Nnww&ixlib=rb-4.0.3&q=80&w=1080" />
                         <img class="slide" src="https://images.unsplash.com/photo-1496171367470-9ed9a91ea931?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwxM3x8dGVjaG5vbG9neXxlbnwwfHx8fDE3MTgyMjE1ODR8MA&ixlib=rb-4.0.3&q=80&w=1080" />
                     </div>
-                    <div class="slide-box">
+                    <div class="[transform:rotateZ(15deg)_translateX(-33%)] flex flex-col w-max justify-center items-center gap-y-6 mr-6">
                         <img class="slide" src="https://images.unsplash.com/photo-1507608869274-d3177c8bb4c7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHw1Nnx8c3VtbWVyfGVufDB8fHx8MTcxODIwNDM2N3ww&ixlib=rb-4.0.3&q=80&w=1080" />
                         <img class="slide" src="https://images.unsplash.com/photo-1548111396-12bc79c82f18?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHw1Mnx8VFJBSU4lMjBGRU1BTEUlMjBTSVRUSU5HfGVufDB8fHx8MTcxODMwNDA4Mnww&ixlib=rb-4.0.3&q=80&w=1080" />
                         <img class="slide" src="https://images.unsplash.com/photo-1569317002804-ab77bcf1bce4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwxfHx1bnNwbGFzaHxlbnwwfHx8fDE3MTgxOTgyNzN8MA&ixlib=rb-4.0.3&q=80&w=1080" />
@@ -38,32 +31,12 @@
                         <img class="slide" src="https://images.unsplash.com/photo-1621329109234-d7657d2f3846?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHw0N3x8Y3J5c3RhbHxlbnwwfHx8fDE3MTgyMTc0NzJ8MA&ixlib=rb-4.0.3&q=80&w=1080" />
                         <img class="slide" src="https://images.unsplash.com/photo-1483347756197-71ef80e95f73?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwxfHxub3J0aGVybiUyMGxpZ2h0c3xlbnwwfHx8fDE3MTgyMjE3OTV8MA&ixlib=rb-4.0.3&q=80&w=1080" />
                     </div>
-<!--                     <div class="slide-box">
-                        <img class="slide NOLIVE" src="https://images.unsplash.com/photo-1491929007750-dce8ba76e610?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHw0MHx8c3VtbWVyfGVufDB8fHx8MTcxODIwNDI0Nnww&ixlib=rb-4.0.3&q=80&w=1080"  />
-                        <img class="slide NOLIVE" src="https://images.unsplash.com/photo-1569317002804-ab77bcf1bce4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwxfHx1bnNwbGFzaHxlbnwwfHx8fDE3MTgxOTgyNzN8MA&ixlib=rb-4.0.3&q=80&w=1080" />
-                        <img class="slide NOLIVE" src="https://images.unsplash.com/photo-1569317002804-ab77bcf1bce4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwxfHx1bnNwbGFzaHxlbnwwfHx8fDE3MTgxOTgyNzN8MA&ixlib=rb-4.0.3&q=80&w=1080" />
-                        <img class="slide NOLIVE" src="https://images.unsplash.com/photo-1627667928346-5fc86d099a5c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHw0N3x8c3BvcnQlMjBjYXJ8ZW58MHx8fHwxNzE4MTk2MTE3fDA&ixlib=rb-4.0.3&q=80&w=1080" />
-                        <img class="slide NOLIVE" src="https://images.unsplash.com/photo-1627667928346-5fc86d099a5c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHw0N3x8c3BvcnQlMjBjYXJ8ZW58MHx8fHwxNzE4MTk2MTE3fDA&ixlib=rb-4.0.3&q=80&w=1080" />
-                        <img class="slide NOLIVE" src="https://images.unsplash.com/photo-1627667928346-5fc86d099a5c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHw0N3x8c3BvcnQlMjBjYXJ8ZW58MHx8fHwxNzE4MTk2MTE3fDA&ixlib=rb-4.0.3&q=80&w=1080" />
-                    </div> -->
-<!--                     <div class="slide-box">
-                        <img class="slide" src="https://images.unsplash.com/photo-1448375240586-882707db888b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwxfHxmb3Jlc3R8ZW58MHx8fHwxNzE4MTIzNTQyfDA&ixlib=rb-4.0.3&q=80&w=1080" />
-                        <img class="slide" src="https://images.unsplash.com/photo-1503435980610-a51f3ddfee50?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwzfHxmb3Jlc3R8ZW58MHx8fHwxNzE4MTIzNTQyfDA&ixlib=rb-4.0.3&q=80&w=1080" />
-                        <img class="slide" src="https://images.unsplash.com/photo-1448375240586-882707db888b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwxfHxmb3Jlc3R8ZW58MHx8fHwxNzE4MTIzNTQyfDA&ixlib=rb-4.0.3&q=80&w=1080" />
-                        <img class="slide" src="https://images.unsplash.com/photo-1503435980610-a51f3ddfee50?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwzfHxmb3Jlc3R8ZW58MHx8fHwxNzE4MTIzNTQyfDA&ixlib=rb-4.0.3&q=80&w=1080" />
-                    </div>
-                    <div class="slide-box">
-                        <img class="slide" src="https://images.unsplash.com/photo-1448375240586-882707db888b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwxfHxmb3Jlc3R8ZW58MHx8fHwxNzE4MTIzNTQyfDA&ixlib=rb-4.0.3&q=80&w=1080" />
-                        <img class="slide" src="https://images.unsplash.com/photo-1503435980610-a51f3ddfee50?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwzfHxmb3Jlc3R8ZW58MHx8fHwxNzE4MTIzNTQyfDA&ixlib=rb-4.0.3&q=80&w=1080" />
-                        <img class="slide" src="https://images.unsplash.com/photo-1448375240586-882707db888b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwxfHxmb3Jlc3R8ZW58MHx8fHwxNzE4MTIzNTQyfDA&ixlib=rb-4.0.3&q=80&w=1080" />
-                        <img class="slide" src="https://images.unsplash.com/photo-1503435980610-a51f3ddfee50?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTAyNjl8MHwxfHNlYXJjaHwzfHxmb3Jlc3R8ZW58MHx8fHwxNzE4MTIzNTQyfDA&ixlib=rb-4.0.3&q=80&w=1080" />
-                    </div> -->
                 </div>
             </div>
         </section>
 
         <section class="">
-            <div class="contentbox text-white bg-[#cccf] py-8 px-4 backdrop-blur-lg backdrop-contrast-75">
+            <div class="shadow-inner-[0_0_0.5rem_0.12rem_#222] shadow-[0_0_0.5rem_0.2rem_#111] relative min-w-[80vw] text-white bg-[#ccc1] py-8 px-4 my-0 mx-auto border-solid border-y-[0.75rem] border-[#eee4]   backdrop-blur-lg backdrop-contrast-75">
                 <h1 class="main-title text-6xl font-semibold font-mono text-center mt-8 mb-12"> 3PhotoLib </h1>
                 <h2 class="main-title-alt text-xl font-mono text-center my-4"> The royalty-free photo delivery platform for free-forever use! </h2>
                 <div class="mt-8 mb-4 min-w-24 max-w-[40vw] min-h-10 bg-[#111a] rounded-full mx-auto"> 
@@ -76,69 +49,12 @@
 </template>
 
 <style scoped>
-body {
-  margin: 0 auto;
-}
-
-.board {
-  overflow: hidden;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.slide-container {
-  display: grid;
-  place-items: center;
-  width: max(max-content, 100vw);
-  background: linear-gradient(45deg, #eee, #ddd);
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: auto;
-  gap: 1.5rem;
-  max-height: 100dvh;
-}
-
-.slide-box {
-  transform: rotateZ(15deg) translateX(-33%);
-  display: flex;
-  width: max-content;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  row-gap: 1.5rem;
-  margin-right: 1.5rem;
-}
 
 .slide {
-  width: max(32vw, 32vh);
-  height: max(18vw, 18vh);
-  box-shadow: .2rem .2rem .85rem #111;
-  border-radius: 5%;
-}
-
-
-/* */
-
-.sectest {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100dvh;
-}
-
-
-.contentbox {
-    position: relative;
-    background: #eee0;
-    min-width: 80vw;
-    margin: 0 auto;
-    border-block: .75rem solid #eee4;
-    box-shadow: inset 0 0 .5rem .12rem #222, 0 0 .5rem .2rem #111;
-/*     position: absolute;
-    top: 50%;
-    left: 10%; */
-    
+    width: max(32vw, 32vh);
+    height: max(18vw, 18vh);
+    box-shadow: .2rem .2rem .85rem #111;
+    border-radius: 5%;
 }
 
 .main-title {
