583d45dc5422095a7e9088ed61b769f0aaac98fc
Martimex <kamilmarciniak533@wp.pl>
2024-11-15 22:02:03 +0100

    fix: minor fix for fallback UI elements
    Added a bottom padding for fallback UI sections
    to prevent them from sticking too close from the
    bottom-side action panels in some specific cases.

--- components/EmptyResponses/NoLikedPhotos.vue
+++ components/EmptyResponses/NoLikedPhotos.vue
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 </script>
 
 <template>
-    <section class="transition-opacity duration-500 delay-200 flex flex-col items-center justify-center">
+    <section class="transition-opacity duration-500 delay-200 flex flex-col items-center justify-center pb-20">
         <FontAwesomeIcon :icon="faHeart" class="text-9xl text-gray-300 my-10"/>
         <div class="mx-6 text-gray-400 flex flex-col items-center gap-y-4 text-center">
             <p class="text-base"> Seems that you have not added any liked photos... yet! </p>
