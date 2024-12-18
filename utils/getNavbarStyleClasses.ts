6d7dbcc6e4e14048c1bd935501d17e18449e3a76
Martimex <kamilmarciniak533@wp.pl>
2024-12-18 17:15:50 +0100

    fix: correct top navbar sizing & solve blur issue
    A quick follow-up for the latest patch. The
    top-navbar now has intended and consistent
    sizing (no more content reflows!) and also
    corrected again the blur problem, when bottom
    panel were injected into the top one (for large
    screens).

--- utils/getNavbarStyleClasses.ts
+++ utils/getNavbarStyleClasses.ts
@@ -1,7 +1,7 @@
 export const getNavbarStyleClasses = function(navbarPositionShorthand: string, disableBackdropBlur: boolean = false): string {
     let navbarPositionClasses = getNavbarPositionClasses(navbarPositionShorthand);
-    return 'left-0 right-0 text-[#333] bg-[#fff5] py-5 mx-auto max-w-[1320px]' 
-        + ' ' + (disableBackdropBlur === false && `backdrop-blur`)
+    return 'left-0 right-0 text-[#333] mx-auto max-w-[1320px]' 
+        + ' ' + (disableBackdropBlur === false && `backdrop-blur bg-[#fff5] py-5 lg:py-8`)
         + ' ' + navbarPositionClasses;
 }
 
